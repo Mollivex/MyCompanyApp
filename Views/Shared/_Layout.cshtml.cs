c19ef9ed1d5000da3fefac47b28b7a9741a82716
Alex <licoms1997@gmail.com>
2023-08-12 18:51:01 +0300

    Add HTML5 template files + CKeditor

---
+++ Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCompanyApp.Views.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
