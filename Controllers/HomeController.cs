198493cf2ca47e8c5e85a37f38ed2cfa82af7c46
Alex <licoms1997@gmail.com>
2023-08-12 22:57:38 +0300

    Add HomeController controller and Index view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompanyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
