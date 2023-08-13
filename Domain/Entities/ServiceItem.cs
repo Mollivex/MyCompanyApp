92c8ec8544b01f968fe566b6bd5a30ee03b518c3
Alex <licoms1997@gmail.com>
2023-08-13 15:29:33 +0300

    Modify ServiceItem.cs

--- Domain/Entities/ServiceItem.cs
+++ Domain/Entities/ServiceItem.cs
@@ -1,6 +1,17 @@
-﻿namespace MyCompanyApp.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompanyApp.Domain.Entities
 {
     public class ServiceItem : EntityBase
     {
+        [Required(ErrorMessage="Fill service name")]
+        [Display(Name = "Service name")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Service short description")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Service full description")]
+        public override string Text { get; set; }
     }
 }
