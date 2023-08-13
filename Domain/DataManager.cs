49b306c4e7cd219dffe11c6276843c9318065e55
Alex <licoms1997@gmail.com>
2023-08-13 16:41:47 +0300

    Create auxillary class DataManager.cs in /Domain to add entry point to the contexts

---
+++ Domain/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using MyCompanyApp.Domain.Repositories.Abstract;
+
+namespace MyCompanyApp.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields {  get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+        public DataManager (ITextFieldsRepository textFieldRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
