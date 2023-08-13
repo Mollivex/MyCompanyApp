8bae511874cca984bded2a30929c6cd676c4d8d2
Alex <licoms1997@gmail.com>
2023-08-13 15:26:14 +0300

    Create AppDbContext.cs to connect application with database

---
+++ Domain/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyCompanyApp.Domain.Entities;
+
+/*
+ *      In this AppDbContext class inherited from IdentityDbContext<IdentityUser> 
+ * we create database context to connect application with database.
+ */
+
+namespace MyCompanyApp.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TextField> TextFields { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "8asdf1818-b019-j184-f48h3h32e3qd2",
+                Name = "admin",
+                NormalizedName = "ADMIN"
+            });
+
+            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = "8as4f1148-g029-je92-f401ow3us6e3qd2",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "my@email.com",
+                NormalizedEmail = "MY@EMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "superpassword"),
+                SecurityStamp = string.Empty,
+
+                
+
+            });
+            
+        }
+
+    }
+}
