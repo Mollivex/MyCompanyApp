a1ceb1eb2e394c894c3effd4acc17a8f411cac21
Alex <licoms1997@gmail.com>
2023-08-14 22:57:46 +0300

    Add HomeController.cs to /Area/Admin

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompanyApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+    }
+}
